88eb790f054bfc67250ed4513d24434b2530fcb2
Curtis Stilwell <3722142+Curtis-Stilwell@users.noreply.github.com>
2026-01-05 11:12:30 -0500

    Initial Commit

---
+++ Hiya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hiya
+//
+//  Created by Curtis Stilwell on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Curtis Stilwell on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
